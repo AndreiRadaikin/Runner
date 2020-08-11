47cd4237e0535548f3dc95d3629b688eee0b4b0b
a.radaikin <andrey.radaykin@gmail.com>
2020-08-11 19:02:44 +0300

    Fixed jump bug in MovementService

--- Assets/MovementService.cs
+++ Assets/MovementService.cs
@@ -4,10 +4,11 @@ public class MovementService : MonoBehaviour
 {
     private Rigidbody rb;
 
-    private Vector3 sideForce = new Vector3(0.0f, 0.0f, 20f);
-    public Vector3 forwardForce = new Vector3(100f, 0.0f, 0.0f);
-    public Vector3 jumpForce = new Vector3(0.0f, 100f, 0.0f);
-    private bool isGtounded = true;
+    private Vector3 sideForce = new Vector3(0.0f, 0.0f, 0.1f);
+    private Vector3 forwardForce = new Vector3(300f, 0.0f, 0.0f);
+    private Vector3 jumpForce = new Vector3(0.0f, 4f, 0.0f);
+
+    private bool isGrounded = true;
 
 
     void Start()
@@ -16,38 +17,37 @@ public class MovementService : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         rb.detectCollisions =true;
+        rb.isKinematic = false;
        
     }
 
-    void OnCollisionStay()
+    private void Update()
     {
-        isGtounded = true;
-    }
+        if (Input.GetKey("a"))
+        {
 
-
-    void FixedUpdate()
-    {
-        rb.AddForce(forwardForce * Time.deltaTime, ForceMode.Force);
-
-        if (Input.GetKey("a")) {
-
-            rb.AddForce(sideForce * Time.deltaTime, ForceMode.VelocityChange);
+            rb.AddForce(sideForce, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey("d"))
         {
-            rb.AddForce(-sideForce * Time.deltaTime, ForceMode.VelocityChange);
+            rb.AddForce(-sideForce, ForceMode.VelocityChange);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGtounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            isGtounded = false;
-            Debug.Log("Jump");
-            rb.AddForce(jumpForce * Time.deltaTime, ForceMode.VelocityChange);
+            rb.AddForce(jumpForce, ForceMode.VelocityChange);
+            isGrounded = false;
         }
-
     }
 
-  
-    
+    void FixedUpdate()
+    {
+        rb.AddForce(forwardForce * Time.deltaTime, ForceMode.Force);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
 }
